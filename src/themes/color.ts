366ae69a874ee970ec73fa00c1bdc3804927d16a
TTCode10 <ttcode10@gmail.com>
2020-12-21 20:24:16 +0800

    🎨  Iteration 1 / Set theme colors

---
+++ src/themes/color.ts
@@ -0,0 +1,11 @@
+const color = {
+  redPrimary: "#E4002B",
+  greyPrimary: "#333F48",
+  greySecondary: "#697484",
+  greyTertiary: "#8E9397",
+  border: "#D2D5DA",
+  background: "#F0F2F5",
+  white: "#FFFFFF",
+};
+
+export default color;
