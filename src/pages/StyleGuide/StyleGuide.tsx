8114a57fcd3e729ef6e9abc99df747c9d61e1dc5
TTCode10 <ttcode10@gmail.com>
2020-12-22 23:29:20 +0800

    💄 Iteration 1 / Integrated various components into style guide page

--- src/pages/StyleGuide/StyleGuide.tsx
+++ src/pages/StyleGuide/StyleGuide.tsx
@@ -12,26 +12,37 @@ import {
   Header,
 } from '../../components';
 
+const Container = styled.div`
+  padding: 0 12.5rem;
+`;
+
 const Wrapper = styled.div`
   width: 300px;
   height: 200px;
+`;
+
+const CardItem = styled(Card)`
+  overflow-y: auto;
 `;
 
 const StyleGuide: React.FC<Record<string, unknown>> = () => {
   return (
     <>
       <Header />
-      <H1Text>Style Guide Page</H1Text>
-      <H1Text>H1 Text</H1Text>
-      <H2Text>H2 Text</H2Text>
-      <H3Text>H3 Text</H3Text>
-      <P1Text>P1 Text</P1Text>
-      <P2Text>P2 Text</P2Text>
-      <P3Text>P3 Text</P3Text>
-      <Button>Button</Button>
-      <Wrapper>
-        <Card />
-      </Wrapper>
+      <Container>
+        <H1Text>Style Guide Page</H1Text>
+        <Wrapper>
+          <CardItem>
+            <H1Text>H1 Text</H1Text>
+            <H2Text>H2 Text</H2Text>
+            <H3Text>H3 Text</H3Text>
+            <P1Text>P1 Text</P1Text>
+            <P2Text>P2 Text</P2Text>
+            <P3Text>P3 Text</P3Text>
+          </CardItem>
+        </Wrapper>
+        <Button>Button</Button>
+      </Container>
     </>
   );
 };
