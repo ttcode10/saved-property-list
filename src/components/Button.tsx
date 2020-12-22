d95cb275551d2a39daa841465808a914925ef071
TTCode10 <ttcode10@gmail.com>
2020-12-22 21:04:09 +0800

    💄 Iteration 1 / Added button as a component

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+import { color, fontFamily } from '../themes/themes';
+
+const Button = styled.button`
+  font-size: 0.875rem;
+  color: ${color.greyPrimary};
+  font-family: ${fontFamily};
+  text-align: center;
+  border-radius: 4px;
+  border: 1px solid ${color.border};
+  background: none;
+  padding: 0.5rem;
+  cursor: pointer;
+  outline: 0;
+
+  &:hover {
+    background-color: ${color.background};
+  }
+`;
+
+export default Button;
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { H1Text, H2Text, H3Text, P1Text, P2Text, P3Text } from './Typography';
+export { default as Button } from './Button';
