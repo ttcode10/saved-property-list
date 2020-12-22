7a8343a8e894b6a931b8f9086844e9c1e31a3037
TTCode10 <ttcode10@gmail.com>
2020-12-22 21:03:54 +0800

    💄 Iteration 1 / Added typography as a component

---
+++ src/components/Typography.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+import { fontFamily } from '../themes';
+
+export const H1Text = styled.h1`
+  font-family: ${fontFamily};
+  font-size: 1.25rem;
+  font-weight: 500;
+`;
+
+export const H2Text = styled.h2`
+  font-family: ${fontFamily};
+  font-size: 1rem;
+  font-weight: 400;
+`;
+
+export const H3Text = styled.h3`
+  font-family: ${fontFamily};
+  font-size: 0.875rem;
+  font-weight: 300;
+`;
+
+export const P1Text = styled.p`
+  font-family: ${fontFamily};
+  font-size: 1.125rem;
+  font-weight: 500;
+`;
+
+export const P2Text = styled.p`
+  font-family: ${fontFamily};
+  font-size: 1rem;
+  font-weight: 400;
+`;
+
+export const P3Text = styled.p`
+  font-family: ${fontFamily};
+  font-size: 0.875rem;
+  font-weight: 100;
+`;
