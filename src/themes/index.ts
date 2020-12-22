6ac14a2a8cacd8a334178497851b07ad1c990139
TTCode10 <ttcode10@gmail.com>
2020-12-22 21:02:42 +0800

    🚚 Iteration 1 / Created color and font family in themes.tsx

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./color";
+export { color, fontFamily } from './themes';
---
+++ src/themes/themes.ts
@@ -0,0 +1,12 @@
+export const color = {
+  redPrimary: '#E4002B',
+  greyPrimary: '#333F48',
+  greySecondary: '#697484',
+  greyTertiary: '#8E9397',
+  border: '#D2D5DA',
+  background: '#F0F2F5',
+  white: '#FFFFFF',
+};
+
+export const fontFamily =
+  "'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Helvetica Neue', sans-serif";
